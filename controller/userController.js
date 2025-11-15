37545fdd5ba93de2902e2d9b3062ce442bbc96d0
HelloAyan <helloayan171@gmail.com>
2025-11-16 02:32:51 +0600

    feat: user update api created

--- controller/userController.js
+++ controller/userController.js
@@ -26,3 +26,31 @@ export const createUser = async (req, res) => {
         res.status(500).json({ error: "Internal Server Error" });
     }
 }
+
+export const allUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        if (users.length === 0) {
+            return res.status(404).json({ message: "No users found." });
+        }
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ error: "Internal Server Error" });
+    }
+}
+
+export const updateUser = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const exitUser = await User.findById(id);
+        if (!exitUser) {
+            return res.status(404).json({ message: "User not found." })
+        }
+        const updateUser = await User.findByIdAndUpdate(id, req.body, { new: true });
+        res.status(200).json(updateUser);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+}
--- route/userRoute.js
+++ route/userRoute.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { getUser, createUser } from '../controller/userController.js';
+import { getUser, createUser, allUsers, updateUser } from '../controller/userController.js';
 
 const route = express.Router();
 
 route.get('/fetch', getUser);
-route.post('/create', createUser)
+route.post('/create', createUser);
+route.get('/all-users', allUsers);
+route.put('/update/:id', updateUser);
 
 export default route;
