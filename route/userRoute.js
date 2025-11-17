f4b9e0e5e3ca3bf33ef4f7224e0328911fa5ca83
HelloAyan <helloayan171@gmail.com>
2025-11-17 16:09:35 +0600

    update and delete api created

--- route/userRoute.js
+++ route/userRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getUser, createUser, allUsers, updateUser } from '../controller/userController.js';
+import { getUser, createUser, allUsers, updateUser, deleteUser } from '../controller/userController.js';
 
 const route = express.Router();
 
@@ -7,5 +7,6 @@ route.get('/fetch', getUser);
 route.post('/create', createUser);
 route.get('/all-users', allUsers);
 route.put('/update/:id', updateUser);
+route.delete('/users/:id', deleteUser);
 
 export default route;
